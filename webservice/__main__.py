522c80bb856af4150f3cb892a6fc758e4948b507
Andy <adc4392@gmail.com>
2018-05-10 09:56:24 -0500

    derp

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -15,8 +15,8 @@ async def issue_opened_event(event, gh, *args, **kwargs):
     url = event.data['issue']['comments_url']
     author = event.data['issue']['user']['login']
 
-    message = f'Thanks for the report @{author}! I will look into it ASAP! (I\'m a bot).'
-    await gh.post(url, data={'body']: message)
+    message = f'Thanks for the report @{author}! I will look into it ASAP!.'
+    await gh.post(url, data={'body': message})
 
 
 async def main(request):
